26f03148b2e47cd4dc8cd0187e808b6ac176044b
unknown <Minskoleopc>
2024-02-11 10:26:01 +0530

    added

---
+++ scriptR.js
@@ -0,0 +1,200 @@
+// let x = 10
+// console.log(x)
+// x = 100
+// console.log(x)
+
+// const h = 100 
+// console.log(h)
+// //h = 300
+
+
+// // Arithmetic operation
+
+
+// let a = 10 
+// let b = 5
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+
+// let q = 9
+// let r = 3
+
+// console.log(q+r)
+// console.log(q-r)
+// console.log(q*r)
+// console.log(q/r)
+// console.log(q%r)
+
+
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(6,3)
+// Calculator(10,5)
+// Calculator(8,4)
+
+
+
+// Day 2 
+
+// // function without parameter and without return type 
+
+// function addA(){
+//     console.log(9+9)
+// }
+// addA()
+// addA()
+// addA()
+// addA()
+
+// // function with parameter and without return type 
+// function addB(x,y){
+//     console.log(x+y)
+// }
+// addB(12,3)
+// addB(12,1)
+// addB(12,4)
+
+// // function with parameter and with return type 
+
+// function addC(x,y){
+//     return x + y
+// }
+// let r = addC(13,4)
+// console.log(r)
+// console.log(r + r)
+// console.log(r - r)
+// console.log(r / 4)
+
+// Day 3 ---- type , comparision , logical operator
+
+// Introvert // Extrovert 
+// Calm            loud 
+// less outing     more outing
+// less social     more social
+
+
+// Human 
+// Propeties - color , weight , height 
+// Methods- walk() , talk()
+
+// Vehicle
+// Properties - color , weight , height 
+// Methods - start() , stop()
+
+
+let a = 10 
+console.log(a)
+console.log(typeof a)
+// 10 , - 10 , 10.6 , -10.8, 0.0
+
+let a1  = true
+console.log(a1)
+console.log(typeof a1)
+// true or false
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+// "chinmay", "a", "A", "3"," "," asdfasfsaf@#$wrfewr"
+
+// comparision
+console.log(typeof '10')
+console.log(typeof 10)
+
+// < , > ,<= , >= 
+// == , !=       ----------> value 
+// === , !==     ----------> type and value
+// entity < entity ====> boolean ====> true or false
+
+
+console.log(10 === 10)
+console.log(10 === '10')
+console.log(10 !== '10')
+console.log(10 =='10')
+console.log(10 !='10')
+console.log(10 != 10)
+console.log(7 < 4)
+console.log(7 > 4)
+console.log(7 >= 4)
+console.log(7 <= 4)
+console.log(7 >= 7)
+console.log(7 <= 7)
+
+// && - AND
+
+// true   &&    true  =====> true
+// false  &&    true  =====> false
+// true   &&    false =====> false
+// false  &&    false =====> false
+
+console.log(7 == 7 && 8 == 8)
+console.log(7 == '7' && 8 == 8)
+console.log(7 === '7' && 8 == 8)
+console.log(7 === 7 && 8 != 8)
+console.log(7 !== 7 && 8 !== 8)
+
+// || - OR
+
+// true   ||   true  =====> true
+// false  ||    true  =====> true
+// true   ||    false =====> true
+// false  ||    false =====> false
+
+
+console.log(7 == 7 || 8 == 8)
+console.log(7 == '7' || 8 == 8)
+console.log(7 === '7' || 8 == 8)
+console.log(7 === 7 || 8 != 8)
+console.log(7 !== 7 || 8 !== 8)
+
+// ! - not
+console.log(!(8 ==8))
+console.log(!(8 != 8))
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
