4e8735f232929235e60786a799843a99e559d848
unknown <Minskoleopc>
2024-02-18 18:37:09 +0530

    added

---
+++ script9.js
@@ -0,0 +1,89 @@
+
+// push()
+// action 
+// return type 
+
+let names = ["chinmay","sham","ram","satish"]
+let q1 = names.push("sarika")
+console.log(q1)
+console.log(q1)
+// unshift()
+let q2 = names.unshift('poorva')
+console.log(q2)
+console.log(names)
+// pop()
+let fruits = ["apple","mango","banana","grapes"]
+let q3 = fruits.pop()
+console.log(q3)
+console.log(fruits)
+// shift()
+let q4 = fruits.shift()
+console.log(q4)
+console.log(fruits)
+
+// indexOf()
+let animals  =["tiger","lion","bear","cat"]
+let q5  = animals.indexOf('lion')
+console.log(q5)
+let q6  = animals.indexOf('Lion')
+console.log(q6)
+
+// includes()
+let city = ["banglore","kolkata","chennai","nagpur"]
+let q7 = city.includes('Chennai')
+console.log(q7)
+
+// reverser()
+let q8 = city.reverse()
+console.log(city)
+console.log(q8)
+// at()
+//               0         1           2           3
+let country = ["india","pakistan","bangladesh","srilanka"]
+let q9 = country.at(2)
+console.log(q9)
+
+// concat()
+
+let a = [11,22,33]
+let b = [44,55,66]
+
+let q10 =  a.concat(b)
+console.log(q10)
+console.log(a)
+console.log(b)
+
+// join()
+let info  = ["chinmay","deshpande",7709192441]
+let q11 = info.join('-')
+let q12 = info.join('@')
+console.log(q11)
+console.log(q12)
+
+
+// ------------------------------------->
+
+
+
+
+// map()
+
+// filter()
+
+// forEach()
+
+// reduce()
+
+// every()
+
+// some()
+
+// find()
+
+// findIndex()
+
+
+
+
+
+
