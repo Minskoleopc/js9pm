e686b218d1ae536f391fb2e5f9f00a545f5087e9
unknown <Minskoleopc>
2024-02-19 21:23:21 +0530

    added

--- script9.js
+++ script9.js
@@ -63,6 +63,71 @@ console.log(q12)
 
 // ------------------------------------->
 
+//                 0     1    2   3
+let birthYear  = [1989,1990,1991,1992]
+let ages = [] 
+
+// [2023 -1989,2023 -1990,2023 -1991 , 2023 -1992]
+// [35,34,33,32]
+
+for(let i = 0 ; i < birthYear.length ; i++){
+    // console.log(i)
+    // console.log(birthYear[i])
+    //console.log(2024 - birthYear[i])
+    let x = 2024 - birthYear[i]
+    ages.push(x)
+}
+console.log(ages)
+
+
+// program 2
+//           0  1   2  3  4  5 6
+let marks = [11,44,66,88,33,44,55]
+let above50 = []
+// [66,88,55]
+
+for(let i = 0 ; i < marks.length ; i++){
+    //console.log(i)
+    //console.log(marks[i])
+    if(marks[i] > 50){
+        //console.log(marks[i])
+        above50.push(marks[i])
+    }
+}
+console.log(above50)
+
+// program3
+
+let numbers = [11,22,33] // 66
+let total = 0
+for(let i = 0 ; i < numbers.length ; i++){
+    //console.log(i)
+    //console.log(numbers[i])
+    total = total + numbers[i]
+    //       0    +      11   ------>11
+    //       11   +      22   ------>33
+    //       33   +      33   -------> 66
+}
+console.log(total)
+
+// program 4
+//              0          1         2        3
+let cities  =["nagpur","chennai","banglore","kolkata"]
+for(let i = 0 ; i < cities.length ; i++){
+    //console.log(i)
+    console.log("welcome "+cities[i])
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
