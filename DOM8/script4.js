6bb78bba7abff82dcab5aac34922082eb1aa6cd8
unknown <Minskoleopc>
2024-03-13 21:36:58 +0530

    added

---
+++ DOM8/script4.js
@@ -0,0 +1,18 @@
+
+let ulList = document.querySelector('ul')
+console.log(ulList)
+
+let inputText = document.querySelector('input')
+console.log(inputText)
+
+let buttonA = document.querySelector('button')
+console.log(buttonA)
+
+buttonA.addEventListener('click',function(){
+    let txt = inputText.value
+    let newEle = document.createElement('li') // <li></li>
+    newEle.textContent = txt // <li>Papaya</li>
+    ulList.appendChild(newEle)
+    inputText.value = ""
+    
+})
