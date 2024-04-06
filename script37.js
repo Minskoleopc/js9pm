22cecba110ded3426ef23cbe14639ff2b45a1077
unknown <Minskoleopc>
2024-04-06 21:35:28 +0530

    added

---
+++ script37.js
@@ -0,0 +1,70 @@
+// program 1
+console.log("hello")
+
+
+
+// program 2
+
+var firsName = "vijeet"
+var lastName = "dani"
+// console.log(window.firstName)
+// console.log(window.lastName)
+
+// let amol = {
+//     firsName:"amol",
+//     lastName:"rao",
+//     displayName:function(){
+//         // this --- amol
+//         console.log(this.firsName + this.lastName) // amolrao
+//         let displayName2 = function(){
+//             // this ---> window
+//             console.log(this.firsName + this.lastName) // vijeetdani
+//         }
+//         displayName2()
+//     }
+// }
+// amol.displayName()
+
+
+// program 2
+// let amol = {
+//     firsName:"amol",
+//     lastName:"rao",
+//     displayName:function(){
+//         // this --- amol
+//         console.log(this.firsName + this.lastName) // amolrao
+//         let displayName2 = () =>{
+//             // this - amol
+//             console.log(this.firsName + this.lastName) // amolrao
+//         }
+//         displayName2()
+//     }
+// }
+// amol.displayName()
+
+
+
+// let  amol = {
+//     firsName:"amol",
+//     lastName:"rao",
+//     displayName:()=>{
+//         // this --- window
+//         console.log(this.firsName + this.lastName) // 
+//         let displayName2 = () =>{
+//             // this - window
+//             console.log(this.firsName + this.lastName) // 
+//         }
+//         displayName2()
+//     }
+// }
+// amol.displayName()
+
+
+
+
+
+
+
+
+
+
