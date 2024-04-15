962af31e4e7627e2c7e5e89734299440a27bf538
unknown <Minskoleopc>
2024-04-15 22:59:16 +0530

    added

---
+++ AJAX/day11.js
@@ -0,0 +1,55 @@
+
+
+//getUsersPagesWise
+
+function getUsers(pageNumber) {
+    return fetch(`https://reqres.in/api/users?page=${pageNumber}`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (response) {
+            return response.data[0].id
+        })
+}
+
+//getUserID
+function getID(id) {
+    return fetch(`https://reqres.in/api/users/${id}`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (response) {
+            return response.data
+        })
+}
+// renderHtml
+function renderHtml(element) {
+    document.write(`<h1>${element.first_name}<h1>`)
+    document.write(`<h1>${element.last_name}<h1>`)
+    document.write(`<h1>${element.email}<h1>`)
+    document.write(`<h1>${element.id}<h1>`)
+    document.write(`<img src = ${element.avatar}>`)
+}
+
+// getUsers(2)
+// .then(function(id){
+//     return getID(id)
+// })
+// .then(function(element){
+//     renderHtml(element)
+// })
+
+
+// async await
+
+
+
+async function getUsersInfo(pageNumber){
+   let id =  await getUsers(pageNumber)
+   let user  = await getID(id)
+   renderHtml(user)
+
+}
+getUsersInfo(2)
+
+
---
+++ AJAX/day12.js
@@ -0,0 +1,13 @@
+// promise.all()
+
+// promise.allSelttled()
+
+// Promise.race()
+
+// Promise.any()
+
+// .then()
+// async await
+// apis
+
+// 5 pm javascript projects 
